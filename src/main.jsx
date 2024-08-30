d0504c7d1df6433ca8c9ff35dd2f8a4787282fee
Edith <edith-friedgund-johanna.foerster@stud.uni-bamberg.de>
2024-08-30 10:34:50 +0200

    Todo: store setup in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+
+// Wrap the App with the Provider component and pass the Redux store as a prop. 
+// This allows every component access to the details of store.js.
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
