b7925714eca39c0f433c11f0c086c2be95bce4c2
Edith <edith-friedgund-johanna.foerster@stud.uni-bamberg.de>
2024-08-30 09:46:08 +0200

    task2. Todos left in CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,16 +7,45 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
 
-    
+    // Todo:
+    // not sure about this one
+    removeItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const hasItem = state.items.filter(item => item.name !== action.payload);
+      if (hasItem) {
+        hasItem.quantity--;
+      } else {
+       
+      }
     },
+
+
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    },
+
+
   },
+
 });
+
+
+// Todo:
+//  Export the action creators, addItem() to use in ProductList.jsx, removeItem(), and updateQuantity(), to use in the CartItem.jsx.
+   // Also export the reducer as the default to use in store.js.
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
